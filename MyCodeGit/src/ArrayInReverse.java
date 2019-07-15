8aabc2b8f43aeef590cd604e59b60d39d9e365c5
Ajinkya <patil28ajinkya@gmail.com>
2019-07-16 01:23:41 +0530

    adding test file and some updated code

--- MyCodeGit/src/ArrayInReverse.java
+++ MyCodeGit/src/ArrayInReverse.java
@@ -9,6 +9,9 @@ public class ArrayInReverse {
 	public static void reverse(int arr[]){
 		for(int i=arr.length-1;i>=0;i--){
 			System.out.print(" "+arr[i]);
+			System.out.println("aa i just up dated this line i will upload on git hub ");
+			System.out.println("");
+			
 		}
 	}
 	

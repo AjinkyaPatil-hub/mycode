03611a0dc665be2ea06fe1a4af56120ac5c4f035
Ajinkya <patil28ajinkya@gmail.com>
2019-07-16 01:34:00 +0530

    last change

--- MyCodeGit/src/Test.java
+++ MyCodeGit/src/Test.java
@@ -1,6 +1,7 @@
 
 public class Test {
 	public static void main(String[] args) {
-		
+		System.out.println("i made succesfully changes i am doing to just conform again");
+		System.out.println("");
 	}
 }
